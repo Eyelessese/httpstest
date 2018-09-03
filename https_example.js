2c2686468879cff99a79ef299213af97d6a0692b
Ryan Ewanicke <reiz.talsean@gmail.com>
2018-09-02 20:24:55 -0600

    made first https request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,17 @@
 const https = require('https');
 
-console.log('I did it!');
+let options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+let callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
